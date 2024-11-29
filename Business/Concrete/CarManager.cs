27f67bcb364f797ffb9330fbb2c0c64e7b2bdf4b
Kadir Berat Ay <122098770+kadirberatay@users.noreply.github.com>
2024-11-30 02:28:51 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,67 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+	public class CarManager : ICarService
+	{
+		ICarDal _carDal;
+
+		public CarManager(ICarDal carDal)
+		{
+			_carDal = carDal;
+		}
+
+		public IResult Add(Car car)
+		{
+			_carDal.Add(car);
+			return new SuccessResult(Messages.CarAdded);
+		}
+
+		public IResult Delete(Car car)
+		{
+			_carDal.Delete(car);
+			return new SuccessResult(Messages.CarDeleted);
+		}
+
+		public IDataResult<List<Car>> GetAll()
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarsListed);
+		}
+
+		public IDataResult<Car> GetById(int id)
+		{
+			return new SuccessDataResult<Car>(_carDal.Get(p => p.CarId == id));
+		}
+
+		public IDataResult<List<CarDetailDto>> GetCarDetails()
+		{
+			return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
+		}
+
+		public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == brandId));
+		}
+
+		public IDataResult<List<Car>> GetCarsByColorId(int colorId)
+		{
+			return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == colorId));
+		}
+
+		public IResult Update(Car car)
+		{
+			_carDal.Update(car);
+			return new SuccessResult(Messages.CarUpdated);
+		}
+	}
+}
